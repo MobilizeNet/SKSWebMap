801cb955af48a909d812e0392ea823e8423fcd93
devops <devops@mobilize.net>
2024-02-13 12:17:08 -0600

    Updated Code with latest version 55e99da4f21cbfd6de3a02c5133a3028e8da9db6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyMetadata("VersionFileDescription", "")]
 [assembly: ClassInterface(ClassInterfaceType.AutoDual)]
